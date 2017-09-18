756bf3c9dffbd990957578b8f3d76c25d5b4df62
cph-jh331 <cph-jh331@cphbusiness.dk>
2017-09-18 23:57:52 +0200

    day2.tagcounter1

--- day1&2exercises/startCodeForThreadExercises/src/main/java/day2/webscraper/Tester.java
+++ day1&2exercises/startCodeForThreadExercises/src/main/java/day2/webscraper/Tester.java
@@ -1,26 +1,32 @@
 package day2.webscraper;
 
 public class Tester {
-  
-  public static void main(String[] args) {
-    TagCounter tc1 = new TagCounter("http://www.fck.dk");
-    tc1.run();
-    TagCounter tc2= new TagCounter("http://www.google.com");
-    tc2.run();
-    TagCounter tc3= new TagCounter("http://politiken.dk/");
-    tc3.run();
-    
-    System.out.println("Title: "+tc1.getTitle());
-    System.out.println("Div's: "+tc1.getDivCount());
-    System.out.println("Body's: "+tc1.getBodyCount());         
-    
-    
-    System.out.println("Title: "+tc2.getTitle());
-    System.out.println("Div's: "+tc2.getDivCount());
-    System.out.println("Body's: "+tc2.getBodyCount());   
-    
-    System.out.println("Title: "+tc3.getTitle());
-    System.out.println("Div's: "+tc3.getDivCount());
-    System.out.println("Body's: "+tc3.getBodyCount());         
-  }
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        TagCounter tc1 = new TagCounter("http://www.fck.dk");
+        TagCounter tc2 = new TagCounter("http://www.google.com");
+        TagCounter tc3 = new TagCounter("http://politiken.dk/");
+
+        System.out.println("Avilable Processors: " + Runtime.getRuntime().availableProcessors());
+        Thread thread1 = new Thread(tc1);
+        Thread thread2 = new Thread(tc2);
+        Thread thread3 = new Thread(tc3);
+//        long start = System.nanoTime();
+//        tc1.run();
+//        tc2.run();
+//        tc3.run();
+//        long end = System.nanoTime();
+//        System.out.println("Time Sequential: " + (end - start));
+
+        // What is a smart way to check if all the threads are dead?
+        // but parallel is faster.
+        long start = System.nanoTime();
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        long end = System.nanoTime();
+        System.out.println("Time Sequential: " + (end - start));
+
+    }
 }
